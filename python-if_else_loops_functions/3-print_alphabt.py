7ea7a55f63fc6d33fb4905aabd3d81173399b544
xov121 <x.o.v121@gmail.com>
2024-01-25 14:33:33 -0400

    Fixed pycodestyle and string format

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-for char in range(ord('a'), ord('z') + 1):
-    if char != ord('q') and char != ord('e'):
-        print(chr(char), end="")
+for i in range(ord('a'), ord('z') + 1):
+    if i != ord('q') and i != ord('e'):
+        print("{}".format(chr(i)), end="")
