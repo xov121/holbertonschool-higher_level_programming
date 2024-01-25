7184512d64651c1284eef521f10bb0aac2e05925
xov121 <x.o.v121@gmail.com>
2024-01-25 14:50:23 -0400

    Fixed pycodestyle and string format

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def uppercase(str):
-	for char in str:
-		if 'a' <= char <= 'z':
-			char = chr(ord(char) - 32)
-		print(char, end="")
-	print()
+    for char in str:
+        if 'a' <= char <= 'z':
+            char = chr(ord(char) - 32)
+        print("{}".format(char), end="")
+    print()
