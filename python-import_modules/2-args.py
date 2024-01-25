20ec6d129fd97b57e1b46e65f1d5f4ae965f7a44
xov121 <x.o.v121@gmail.com>
2024-01-25 15:31:37 -0400

    Initial commit

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+import sys
+
+
+def main():
+    num_args = len(sys.argv) - 1
+    if num_args == 0:
+        print("0 arguments.")
+    elif num_args == 1:
+        print("1 argument:")
+    else:
+        print("{} arguments:".format(num_args))
+
+    for i in range(1, len(sys.argv)):
+        print("{}: {}".format(i, sys.argv[i]))
+
+
+if __name__ == "__main__":
+    main()
