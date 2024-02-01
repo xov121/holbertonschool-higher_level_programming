0a1fe46809b157d8004c2064adb41ae737ee4ace
xov121 <x.o.v121@gmail.com>
2024-02-01 15:49:56 -0400

    Trying different style of documentation

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,4 +1,24 @@
 #!/usr/bin/python3
+"""
+This module defines the `Square` class.
+
+The `Square` class is an empty class that defines a square. It serves as the
+foundation for more complex behaviors that will be added in subsequent tasks.
+This class is part of a project focused on learning object-oriented
+programming (OOP) and understanding the basics of class creation, attributes,
+methods, and encapsulation in Python.
+
+Classes:
+    Square: Defines a square.
+"""
+
+
 class Square:
-    """An empty Square class"""
+    """
+    An empty class that defines a square.
+
+    At this point, the class doesn't have any attributes
+        or methods. It serves as a
+    starting point for defining squares in later tasks.
+    """
     pass
