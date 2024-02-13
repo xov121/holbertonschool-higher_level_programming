584039efd73835c845fda7b4dd7da96981938b4d
xov121 <x.o.v121@gmail.com>
2024-02-13 07:44:10 -0400

    Added documentation for module and exchanged tabs for spaces in multiples of 4

--- python-more_classes/0-rectangle.py
+++ python-more_classes/0-rectangle.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python3
+"""
+Module 0-rectangle
+Defines an empty class Rectangle
+"""
+
+
 class Rectangle:
-	"""Empty class Rectangle that defines a rectangle"""
-	pass
+    """Empty class Rectangle that defines a rectangle"""
+    pass
