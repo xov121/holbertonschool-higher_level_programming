b8218f49cf178e22201a667f777a7bac046bfbde
xov121 <x.o.v121@gmail.com>
2024-02-20 23:37:33 -0400

    Init commit

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Module that contains the class BaseGeometry.
+"""
+
+
+class BaseGeometry:
+    """
+    A class BaseGeometry with public instance method area.
+    """
+
+    def area(self):
+        """
+        Raises an Exception with a message indicating that the method is not
+        yet implemented.
+        """
+        raise Exception("area() is not implemented")
