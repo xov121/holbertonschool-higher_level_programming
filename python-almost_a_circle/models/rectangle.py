0b0f04ad1355b097dd8e778d2a6dc4354229eac2
xov121 <x.o.v121@gmail.com>
2024-02-21 12:48:05 -0400

    Updated rectangle.py for task 4

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -2,6 +2,7 @@
 """Module for Rectangle class."""
 from models.base import Base
 
+
 class Rectangle(Base):
     """Rectangle class that inherits from Base."""
     def __init__(self, width, height, x=0, y=0, id=None):
