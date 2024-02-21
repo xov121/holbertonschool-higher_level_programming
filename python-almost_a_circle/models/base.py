62a07ec6273f2fc85b58e4ac5d6e579b91578858
xov121 <x.o.v121@gmail.com>
2024-02-21 13:28:53 -0400

    implemented save_to_file method

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -35,3 +35,10 @@ class Base:
                 list_dicts.append(obj.to_dictionary())
         with open(filename, 'w') as f:
             f.write(cls.to_json_string(list_dicts))
+
+    @staticmethod
+    def from_json_string(json_string):
+        """Returns the list of the JSON string representation json_string."""
+        if json_string is None or json_string == "":
+            return []
+        return json.loads(json_string)
