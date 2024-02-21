7b32f0d94dcfcf318b298bc38920a9f03446ad53
xov121 <x.o.v121@gmail.com>
2024-02-20 23:34:51 -0400

    Fixed pycodestyle

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -19,4 +19,4 @@ def inherits_from(obj, a_class):
         True if obj is an instance of a class that inherited from a_class;
         False otherwise, including if obj is a direct instance of a_class.
     """
-    return isinstance(obj, a_class) and type(obj) != a_class
+    return isinstance(obj, a_class) and type(obj) is not a_class
