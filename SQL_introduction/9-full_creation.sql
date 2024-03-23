b8664e5a4875145d29981084a5622bf491ef0c18
xov121 <x.o.v121@gmail.com>
2024-03-23 08:54:55 -0400

    Task 9

---
+++ SQL_introduction/9-full_creation.sql
@@ -0,0 +1,12 @@
+-- Creates second_table in hbtn_0c_0 and inserts multiple rows
+CREATE TABLE IF NOT EXISTS second_table (
+    id INT,
+    name VARCHAR(256),
+    score INT
+);
+
+INSERT INTO second_table (id, name, score) VALUES
+(1, 'John', 10),
+(2, 'Alex', 3),
+(3, 'Bob', 14),
+(4, 'George', 8);
