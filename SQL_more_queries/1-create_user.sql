99e3a20a4dd844dc5665dd6c4029166af582c352
xov121 <x.o.v121@gmail.com>
2024-03-24 06:14:47 -0400

    Task 1

---
+++ SQL_more_queries/1-create_user.sql
@@ -0,0 +1,4 @@
+-- Create user_0d_1 with full privileges
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
+FLUSH PRIVILEGES;
