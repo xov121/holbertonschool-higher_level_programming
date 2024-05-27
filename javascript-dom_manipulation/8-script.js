c255ff335706a517e8d93c9fd2eea59a1a54c440
xov121 <x.o.v121@gmail.com>
2024-05-27 10:26:13 -0400

    script that fetches from URL and displays the value of hello from that fetch in the HTML element with id hello

---
+++ javascript-dom_manipulation/8-script.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function () {
+  fetch('https://hellosalut.stefanbohacek.dev/?lang=fr')
+    .then(response => response.json())
+    .then(data => {
+      document.querySelector('#hello').textContent = data.hello;
+    })
+    .catch(error => console.error('Error:', error));
+});
