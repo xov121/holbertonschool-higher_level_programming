df5eede65cc4472bfdf559f969d139b81fb025fa
xov121 <x.o.v121@gmail.com>
2024-05-27 10:24:11 -0400

    script that fetches the character name from URL

---
+++ javascript-dom_manipulation/6-script.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function () {
+  fetch('https://swapi-api.hbtn.io/api/people/5/?format=json')
+    .then(response => response.json())
+    .then(data => {
+      document.querySelector('#character').textContent = data.name;
+    })
+    .catch(error => console.error('Error:', error));
+});
